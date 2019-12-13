55f81cb73e182db21786aa4966afc6abd8f19f94
Vikram Belde <belvikram@gmail.com>
2019-12-12 21:52:50 -0600

    Updates

---
+++ precache-manifest.fd8994fab7083d49f7501d31894a8738.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38c7679b4f38f2edcd487583a58a7d7",
+    "url": "/super-cart/index.html"
+  },
+  {
+    "revision": "1f7c9c921f41ae043e02",
+    "url": "/super-cart/static/css/main.7f131bbd.chunk.css"
+  },
+  {
+    "revision": "771e56deb50f45924f65",
+    "url": "/super-cart/static/js/2.ccb9f19c.chunk.js"
+  },
+  {
+    "revision": "1f7c9c921f41ae043e02",
+    "url": "/super-cart/static/js/main.22f9c32a.chunk.js"
+  },
+  {
+    "revision": "fbe55a4e29042ba419bd",
+    "url": "/super-cart/static/js/runtime-main.4e710ecc.js"
+  },
+  {
+    "revision": "eb593920f91afa6c9f860d2eae31466a",
+    "url": "/super-cart/static/media/logo.eb593920.png"
+  }
+]);
